f4ee174fa57c53c6e6ed7a73a5f7cb24960af091
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-03-16 04:53:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Reverse the string 
+var str = "Superman";
+var reversedStr = str.split('').reverse().join('');
+console.log(reversedStr);
+
+// ask for the input of the user, and check alert the data type of input
+
+var userInput=parseInt(prompt("Enter Number"));
+var inputType=typeof userInput;
+console.log(inputType);
